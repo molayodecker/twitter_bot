c502a75f8d4ef3cd52f4200d16890be8597095ad
Arthur Decker <molayodecker@gmail.com>
2017-08-26 22:13:27 -0400

    Created a Bot

--- index.js
+++ index.js
@@ -2,25 +2,44 @@ var Twit = require('twit');
 var config = require('./config');
 var T = new Twit(config);
 
-var param = { q: 'finale', count: 2 }
+//tweetIt();
+getTweet();
 
-function datares(err, data, response) {
-  var tweets = data.statuses;
-  for(var key in tweets){
-  	console.log(tweets[key].text);
+function getTweet(){
+  var param = { q: '#GOT', count: 5 }
+
+   T.get('search/tweets',param , datares);
+
+		function datares(err, data, response) {
+			var tweets = data.statuses;
+			for(var key in tweets){
+				console.log(tweets[key].text);
+				console.log('\n');
+			}
+	}
+}
+
+
+function tweetIt(){
+       tweet = { status: '#GOT Jon Snow is the rightful King of Westeros' }
+
+		T.post('statuses/update', tweet, tweeted);
+
+		function tweeted(err, data, response) {
+		   if(err){
+		   console.log("Oops! Something is broken");
+		   }else{
+		   console.log("It's works");
+		   }
+		}
+}
+
+var stream = T.stream('statuses/filter', { track: '#GOT' }, {filter_level: 'medium'})
+
+stream.on('tweet', function (tweet) {
+	var myfilter = tweet;
+  for(var key in myfilter){
+  		console.log(myfilter[key]);
+  		console.log('\n');
   }
-}
-
-T.get('search/tweets',param , datares);
-
-tweet = { status: '#GOT Jon Snow is the rightful King of Westeros' }
-
-function tweeted(err, data, response) {
-   if(err){
-   console.log("Oops! Something is broken");
-   }else{
-   console.log("It's works");
-   }
-}
-
-T.post('statuses/update', tweet, tweeted);
+})
